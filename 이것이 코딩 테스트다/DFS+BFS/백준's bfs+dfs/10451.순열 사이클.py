5ab07520731cb47a16055f8635b0675a6862fb45
jangscon <jtu6568@gmail.com>
2021-10-06 10:19:23 +0900

    Update 10451.순열 사이클.py
    + 순열 사이클

--- 이것이 코딩 테스트다/DFS+BFS/백준's bfs+dfs/10451.순열 사이클.py
+++ 이것이 코딩 테스트다/DFS+BFS/백준's bfs+dfs/10451.순열 사이클.py
@@ -2,16 +2,31 @@ N = int(input())
 
 for _ in range(N) :
     p_num = int(input())
-    p = list(input().split())
+    p = [''] + list(map(int ,input().split()))
 
+    check = [False for _ in range(p_num+1)]
     cycle = 0
-    pocket = []
-
-    for i in range(p_num) :
-        if i+1 == p[i] :
+    for i in range(1, p_num+1) :
+        if p[i] == i :
+            check[i] = True
             cycle += 1
-        else :
-            pocket.append([i+1 , p[i]])
+            continue
+        current_spot = i
+        check_cycle = False
+        while True :
+            if current_spot == i :
+                if check[current_spot] :
+                    break
+                else :
+                    check_cycle = True
+            check[current_spot] = True
+            current_spot = p[current_spot]
+        if check_cycle :
+            cycle += 1
+    print(cycle)
+
+
+
 
 
 
